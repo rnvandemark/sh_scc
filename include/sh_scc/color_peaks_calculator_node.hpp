d1032d268dbfb8fb20c666f66a28a818c69e4ae0
R. Nick Vandemark <rnvandemark@gmail.com>
2022-01-03 19:27:28 -0500

    Gut out use of homography in color peak calculation pipeline.

--- include/sh_scc/color_peaks_calculator_node.hpp
+++ include/sh_scc/color_peaks_calculator_node.hpp
@@ -8,10 +8,7 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "sh_common_interfaces/msg/color.hpp"
 #include "sh_scc_interfaces/msg/color_peaks_telem.hpp"
-#include "sh_scc_interfaces/srv/request_screen_calibration.hpp"
-#include "sh_scc_interfaces/srv/set_screen_calibration_points_of_homography.hpp"
 
-#include <string>
 #include <queue>
 
 namespace sh {
@@ -38,38 +35,16 @@ public:
 class ColorPeaksCalculatorNode : public HeartbeatNode
 {
 protected:
-    const static int WORLD_HEIGHT = 300;
-    const static int WORLD_WIDTH = 400;
-    const static std::vector<cv::Point2f> WORLD_PLANE;
-    const static cv::Size WORLD_SIZE;
-
-    rclcpp::Service<sh_scc_interfaces::srv::RequestScreenCalibration>::SharedPtr
-        screen_calibration_request_srv;
-    rclcpp::Service<sh_scc_interfaces::srv::SetScreenCalibrationPointsOfHomography>::SharedPtr
-        screen_calibration_set_homography_points_srv;
     image_transport::Subscriber cap_image_raw_sub;
     rclcpp::Publisher<sh_common_interfaces::msg::Color>::SharedPtr color_peak_left_pub;
     rclcpp::Publisher<sh_common_interfaces::msg::Color>::SharedPtr color_peak_right_pub;
     rclcpp::Publisher<sh_scc_interfaces::msg::ColorPeaksTelem>::SharedPtr color_peaks_telem_pub;
-
-    cv_bridge::CvImagePtr last_received_frame;
-
-    bool homog_set;
-    cv::Mat homog;
 
     ColorPeakWindow window_left;
     ColorPeakWindow window_right;
 
 public:
     ColorPeaksCalculatorNode();
-
-    void screen_calibration_request_callback(
-        const sh_scc_interfaces::srv::RequestScreenCalibration::Request::SharedPtr req,
-        sh_scc_interfaces::srv::RequestScreenCalibration::Response::SharedPtr res);
-
-    void screen_calibration_set_homography_points_callback(
-        const sh_scc_interfaces::srv::SetScreenCalibrationPointsOfHomography::Request::SharedPtr req,
-        sh_scc_interfaces::srv::SetScreenCalibrationPointsOfHomography::Response::SharedPtr res);
 
     void cap_image_raw_callback(const sensor_msgs::msg::Image::ConstSharedPtr& msg);
 };
--- src/color_peaks_calculator_node.cpp
+++ src/color_peaks_calculator_node.cpp
@@ -2,12 +2,7 @@
 #include "sh_common/ros_names.hpp"
 
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/image_encodings.hpp"
 #include "sh_common_interfaces/msg/point.hpp"
-
-#include <opencv2/photo.hpp>
-#include <opencv2/calib3d.hpp>
-#include <memory>
 
 namespace sh {
 
@@ -62,14 +57,6 @@ void set_best_peak(cv::Mat& labels, cv::Mat& centers, unsigned char& r, unsigned
     }
 }
 
-const std::vector<cv::Point2f> ColorPeaksCalculatorNode::WORLD_PLANE = {
-    cv::Point2f(0,0),
-    cv::Point2f(WORLD_WIDTH,0),
-    cv::Point2f(0,WORLD_HEIGHT),
-    cv::Point2f(WORLD_WIDTH,WORLD_HEIGHT)
-};
-const cv::Size ColorPeaksCalculatorNode::WORLD_SIZE = cv::Size(WORLD_WIDTH, WORLD_HEIGHT);
-
 void ColorPeakWindow::push_and_eval(unsigned char r,
                                     unsigned char g,
                                     unsigned char b,
@@ -101,30 +88,9 @@ void ColorPeakWindow::push_and_eval(unsigned char r,
 
 ColorPeaksCalculatorNode::ColorPeaksCalculatorNode() :
         HeartbeatNode("sh_color_peaks_calculator"),
-        last_received_frame(nullptr),
-        homog_set(false),
-        homog(),
         window_left(),
         window_right()
 {
-    screen_calibration_request_srv = create_service<sh_scc_interfaces::srv::RequestScreenCalibration>(
-        ::sh::names::services::REQUEST_SCREEN_COLOR_CALIBRTION,
-        std::bind(
-            &sh::ColorPeaksCalculatorNode::screen_calibration_request_callback,
-            this,
-            std::placeholders::_1,
-            std::placeholders::_2
-        )
-    );
-    screen_calibration_set_homography_points_srv = create_service<sh_scc_interfaces::srv::SetScreenCalibrationPointsOfHomography>(
-        ::sh::names::services::SET_SCREEN_COLOR_HOMOG_POINTS,
-        std::bind(
-            &sh::ColorPeaksCalculatorNode::screen_calibration_set_homography_points_callback,
-            this,
-            std::placeholders::_1,
-            std::placeholders::_2
-        )
-    );
     cap_image_raw_sub = image_transport::create_subscription(
         this,
         ::sh::names::topics::SCC_CAMERA_IMAGE,
@@ -152,109 +118,20 @@ ColorPeaksCalculatorNode::ColorPeaksCalculatorNode() :
     RCLCPP_INFO(get_logger(), "Initialized.");
 }
 
-void ColorPeaksCalculatorNode::screen_calibration_request_callback(
-    const sh_scc_interfaces::srv::RequestScreenCalibration::Request::SharedPtr req,
-    sh_scc_interfaces::srv::RequestScreenCalibration::Response::SharedPtr res)
-{
-    bool rc = false;
-    cv_bridge::CvImage cv_img_denoised, cv_img_gray, cv_img_blur, cv_img_featin, cv_img_marked;
-    std::vector<cv::Point2f> corners;
-
-    const int max_corners = req->max_corners > 0 ? req->max_corners : 35;
-    const float quality_level = req->quality_level > 0 ? req->quality_level : 0.01;
-    const float min_dist = req->min_dist > 0 ? req->min_dist : 30;
-    const bool do_blur = req->do_blur;
-    const int kh = req->kh > 0 ? req->kh : 3;
-    const int kw = req->kw > 0 ? req->kw : 3;
-
-    if (last_received_frame == nullptr)
-    {
-        RCLCPP_ERROR(get_logger(), "Requested screen calibration, but have not received initial image.");
-        goto END;
-    }
-
-    cv::cvtColor(last_received_frame->image, cv_img_gray.image, cv::COLOR_BGR2GRAY);
-    cv_img_gray.header = last_received_frame->header;
-    cv_img_gray.encoding = sensor_msgs::image_encodings::MONO8;
-    res->intermediate_images.push_back(*(cv_img_gray.toImageMsg()));
-
-    if (do_blur)
-    {
-        cv::blur(cv_img_gray.image, cv_img_blur.image, cv::Size(kw,kh));
-        cv_img_blur.header = cv_img_gray.header;
-        cv_img_blur.encoding = cv_img_gray.encoding;
-        res->intermediate_images.push_back(*(cv_img_blur.toImageMsg()));
-        cv_img_featin = cv_img_blur;
-    }
-    else
-    {
-        cv_img_featin = cv_img_gray;
-    }
-
-    cv::goodFeaturesToTrack(cv_img_featin.image, corners, max_corners, quality_level, min_dist);
-
-    cv_img_marked.image = last_received_frame->image.clone();
-    cv_img_marked.header = last_received_frame->header;
-    cv_img_marked.encoding = last_received_frame->encoding;
-    for (std::vector<cv::Point2f>::iterator iter = corners.begin(); iter != corners.end(); ++iter)
-    {
-        int x = static_cast<int>(iter->x), y = static_cast<int>(iter->y);
-        cv::circle(cv_img_marked.image, cv::Point(x,y), cv_img_marked.image.cols/100, CV_RGB(0,255,0));
-        sh_common_interfaces::msg::Point pt;
-        pt.x = x;
-        pt.y = y;
-        res->corner_positions.push_back(pt);
-    }
-    res->marked_image = *(cv_img_marked.toImageMsg());
-    rc = true;
-
-END:
-    res->corners_found = rc;
-}
-
-void ColorPeaksCalculatorNode::screen_calibration_set_homography_points_callback(
-    const sh_scc_interfaces::srv::SetScreenCalibrationPointsOfHomography::Request::SharedPtr req,
-    sh_scc_interfaces::srv::SetScreenCalibrationPointsOfHomography::Response::SharedPtr res)
-{
-    std::vector<cv::Point2f> pts_image(4);
-    for (unsigned int i = 0; i < req->pts_of_homog.size(); ++i)
-    {
-        pts_image[i] = cv::Point2f(req->pts_of_homog[i].x, req->pts_of_homog[i].y);
-    }
-    homog = cv::findHomography(pts_image, WORLD_PLANE);
-    homog_set = true;
-    res->successful = true;
-}
-
 void ColorPeaksCalculatorNode::cap_image_raw_callback(const sensor_msgs::msg::Image::ConstSharedPtr& msg)
 {
-    last_received_frame = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    if (homog_set)
-    {
         sh_scc_interfaces::msg::ColorPeaksTelem color_peaks_telem_msg;
 
-        color_peaks_telem_msg.intermediate_images.push_back(*(last_received_frame->toImageMsg()));
-
-        cv_bridge::CvImage cv_img_world;
-        cv::warpPerspective(last_received_frame->image, cv_img_world.image, homog, WORLD_SIZE);
-        cv_img_world.header = last_received_frame->header;
-        cv_img_world.encoding = last_received_frame->encoding;
-        color_peaks_telem_msg.intermediate_images.push_back(*(cv_img_world.toImageMsg()));
-
-        cv_bridge::CvImage cv_img_rgb;
-        cv::cvtColor(cv_img_world.image, cv_img_rgb.image, cv::COLOR_BGR2RGB);
-        cv_img_rgb.header = cv_img_world.header;
-        cv_img_rgb.encoding = sensor_msgs::image_encodings::RGB8;
-        color_peaks_telem_msg.world_frame_image = *(cv_img_rgb.toImageMsg());
+        const cv_bridge::CvImageConstPtr img = cv_bridge::toCvShare(msg, msg->encoding);
 
         const int k = 4, attempts = 4;
         double compactness_left, compactness_right;
         cv::Mat kmeans_labels_left, kmeans_labels_right, kmeans_centers_left, kmeans_centers_right;
-        cv::Range range_height(0, cv_img_rgb.image.rows);
-        int width = cv_img_rgb.image.cols;
+        cv::Range range_height(0, img->image.rows);
+        int width = img->image.cols;
         int half_width = width / 2;
-        cv::Mat cv_img_world_left = cv_img_rgb.image(range_height, cv::Range(0, half_width));
-        cv::Mat cv_img_world_right = cv_img_rgb.image(range_height, cv::Range(half_width, width));
+        cv::Mat cv_img_world_left = img->image(range_height, cv::Range(0, half_width));
+        cv::Mat cv_img_world_right = img->image(range_height, cv::Range(half_width, width));
         get_kmeans_of(cv_img_world_left, k, attempts, compactness_left, kmeans_labels_left, kmeans_centers_left);
         get_kmeans_of(cv_img_world_right, k, attempts, compactness_right, kmeans_labels_right, kmeans_centers_right);
 
@@ -273,7 +150,6 @@ void ColorPeaksCalculatorNode::cap_image_raw_callback(const sensor_msgs::msg::Im
         color_peak_left_pub->publish(color_peak_left_msg);
         color_peak_right_pub->publish(color_peak_right_msg);
         color_peaks_telem_pub->publish(color_peaks_telem_msg);
-    }
 }
 
 }
